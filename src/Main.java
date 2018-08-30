e85ee3d9a166eeb1bb396294a3325089e67d180c
Djamel Bousfira <djamel@tomanage.fr>
2018-08-30 10:04:04 +0200

    A) Etiquettes

--- src/Main.java
+++ src/Main.java
@@ -14,22 +14,22 @@ public class Main {
 	public static void main(String[] args) {
 		String file = "./log.txt";
 		FileInputStream in = null, regex = null;
-        BufferedReader readerRegex = null, readerLog = null;
+		BufferedReader readerRegex = null, readerLog = null;
 
 		try {
 			in = new FileInputStream(file);
 			regex = new FileInputStream("./regex.txt");
-            readerRegex = new BufferedReader(new InputStreamReader(regex));
-            readerLog = new BufferedReader(new InputStreamReader(in));
-			
-	        String regexStr = readerRegex.readLine();
+			readerRegex = new BufferedReader(new InputStreamReader(regex));
+			readerLog = new BufferedReader(new InputStreamReader(in));
+
+			String regexStr = readerRegex.readLine();
 			Set<String> namedGroups = getNamedGroup(regexStr);
-			
+
 			Pattern pattern = Pattern.compile(regexStr);
-			
+
 			String log;
-	        BufferedWriter writer = new BufferedWriter(new FileWriter("./output.txt")),
-	        		writerUnmatch = new BufferedWriter(new FileWriter("./unmatch.txt"));
+			BufferedWriter writer = new BufferedWriter(new FileWriter("./output.txt")),
+					writerUnmatch = new BufferedWriter(new FileWriter("./unmatch.txt"));
 			while ((log = readerLog.readLine()) != null) {
 				Matcher matcher = pattern.matcher(log);
 				if (matcher.find())
@@ -38,9 +38,9 @@ public class Main {
 					writerUnmatch.write(log);
 				}							
 			}
-			
-	        writer.close();
-	        writerUnmatch.close();
+
+			writer.close();
+			writerUnmatch.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -54,42 +54,42 @@ public class Main {
 		}
 
 	}
-	
-	private static Set<String> getNamedGroup(String regex) throws Exception {
-        Set<String> namedGroups = new TreeSet<String>();
-        Matcher m = Pattern.compile("\\(\\?<([a-zA-Z][a-zA-Z0-9]*)>").matcher(regex);
 
-        while (m.find()) {
-        	namedGroups.add(m.group(1));
-        }
-        
-        if (!namedGroups.contains("LABEL")) {
-        	throw new Exception("L'expression régulière doit contenir une étiquette LABEL !");
-        }
-        	
-        return namedGroups;
+	private static Set<String> getNamedGroup(String regex) throws Exception {
+		Set<String> namedGroups = new TreeSet<String>();
+		Matcher m = Pattern.compile("\\(\\?<([a-zA-Z][a-zA-Z0-9]*)>").matcher(regex);
+
+		while (m.find()) {
+			namedGroups.add(m.group(1));
+		}
+
+		if (!namedGroups.contains("LABEL")) {
+			throw new Exception("L'expression régulière doit contenir une étiquette LABEL !");
+		}
+
+		return namedGroups;
 	}
-	
+
 	private static void printMatches(Matcher matcher, Set<String> namedGroups, BufferedWriter writer) throws IOException {
-        StringBuilder out = new StringBuilder(), matches = new StringBuilder();
-        
-        out.append(matcher.group("LABEL"));
-        out.append('(');
-        
-        for (String name: namedGroups) {
-        	if (name.equals("LABEL")) continue;
-        	
-            String matchedString = matcher.group(name);
-            if (matchedString != null) {
-            	matches.append(name + "=" + matchedString + ", ");
-            }
-        }
-        
-        out.append(matches.toString().replaceAll(", $", ""));
-        out.append(')');
-        
-        writer.write(out.toString());
-        writer.write('\n');
-    }
+		StringBuilder out = new StringBuilder(), matches = new StringBuilder();
+
+		out.append(matcher.group("LABEL"));
+		out.append('(');
+
+		for (String name: namedGroups) {
+			if (name.equals("LABEL")) continue;
+
+			String matchedString = matcher.group(name);
+			if (matchedString != null) {
+				matches.append(name + "=" + matchedString + ", ");
+			}
+		}
+
+		out.append(matches.toString().replaceAll(", $", ""));
+		out.append(')');
+
+		writer.write(out.toString());
+		writer.write('\n');
+	}
 }
 
